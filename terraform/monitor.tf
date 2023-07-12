b5ac02d5b97e17fdfa230bdd2de394f574630f55
Felix Wolff <wolff.felix@protonmail.com>
2023-07-12 09:55:52 +0200

    feat: completely reusable Terraform for bringing up the app anytime

--- terraform/monitor.tf
+++ terraform/monitor.tf
@@ -18,6 +18,11 @@ resource "digitalocean_database_db" "monitor-db" {
   name       = "rekord-monitor-dev"
 }
 
+resource "digitalocean_database_db" "grafana-db" {
+  cluster_id = digitalocean_database_cluster.postgres-cluster.id
+  name       = "grafana"
+}
+
 resource "digitalocean_database_user" "monitor-db-user" {
   cluster_id = digitalocean_database_cluster.postgres-cluster.id
   name       = "rekord-monitor-dev"
@@ -26,6 +31,11 @@ resource "digitalocean_database_user" "monitor-db-user" {
 resource "digitalocean_database_user" "grafana" {
   cluster_id = digitalocean_database_cluster.postgres-cluster.id
   name       = "grafana-read"
+}
+
+resource "digitalocean_database_user" "grafana-db-user" {
+  cluster_id = digitalocean_database_cluster.postgres-cluster.id
+  name       = "grafana-db-user"
 }
 
 resource "digitalocean_app" "rekor-monitor" {
@@ -38,17 +48,70 @@ resource "digitalocean_app" "rekor-monitor" {
       instance_count     = 1
       instance_size_slug = "basic-xxs"
       http_port          = 3000
+      dockerfile_path    = "Dockerfile.grafana"
+      source_dir         = "/"
 
-      image {
-        registry_type = "DOCKER_HUB"
-        registry      = "grafana"
-        repository    = "grafana-enterprise"
-        tag           = "latest"
+      github {
+        repo           = "flxw/rekor-monitor"
+        branch         = "master"
+        deploy_on_push = true
+      }
+
+      env {
+        key   = "GF_SECURITY_ADMIN_USER"
+        value = var.admin_user_name
+      }
+
+      env {
+        key   = "GF_SECURITY_ADMIN_PASSWORD"
+        value = var.admin_user_password
       }
 
       env {
         key   = "GF_FEATURE_TOGGLES_ENABLE"
         value = "publicDashboards"
+      }
+
+      env {
+        key = "GF_DATABASE_TYPE"
+        value = "postgres"
+      }
+
+      env {
+        key = "GF_DATABASE_URL"
+        value = "postgres://${digitalocean_database_user.grafana-db-user.name}:${digitalocean_database_user.grafana-db-user.password}@${digitalocean_database_cluster.postgres-cluster.host}:${digitalocean_database_cluster.postgres-cluster.port}/${digitalocean_database_db.grafana-db.name}"
+        type = "SECRET"
+      }
+
+      env {
+        key = "GF_DATABASE_SSL_MODE"
+        value = "require"
+      }
+
+      env {
+        key   = "DB_URL"
+        value = "${digitalocean_database_cluster.postgres-cluster.host}:${digitalocean_database_cluster.postgres-cluster.port}"
+        type = "SECRET"
+      }
+
+      env {
+        key   = "DB_USER_NAME"
+        value = digitalocean_database_user.monitor-db-user.name
+      }
+
+      env {
+        key   = "DB_USER_PASSWORD"
+        value = digitalocean_database_user.monitor-db-user.password
+      }
+
+      env {
+        key   = "DB_NAME"
+        value = digitalocean_database_db.monitor-db.name
+      }
+
+      env {
+        key   = "DB_SSL_MODE"
+        value = "require"
       }
     }
 
@@ -68,8 +131,9 @@ resource "digitalocean_app" "rekor-monitor" {
       dockerfile_path    = "Dockerfile"
 
       github {
-        repo   = "flxw/rekor-monitor"
-        branch = "master"
+        repo           = "flxw/rekor-monitor"
+        branch         = "master"
+        deploy_on_push = false
       }
 
       env {
